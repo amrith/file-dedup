b8aff264f73aeb9884d459c300257c0fdd30bf3c
Amrith Kumar <amrith@tesora.com>
2016-08-07 17:31:59 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+A simple file deduplication utility
+
+Will identify files that are potentially duplicates of each other.
+
+Usage:
+
+        file-dedup <pathname>
---
+++ file-dedup/file-dedup.py
@@ -0,0 +1,61 @@
+#
+# a simple utility to find duplicated files
+#
+# Amrith
+#
+
+import os
+import sys
+import hashlib
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+
+    s = os.path.getsize(fname)
+    hash_md5.update(str(s))
+    return hash_md5.hexdigest()
+
+def makepath(root, name):
+    return root + os.sep + name
+
+# compute_hashes
+#
+# returns a dictionary that looks like:
+#
+# { 'hashvalue1': [
+#        { 'name': name, 'size': size },
+#        ... ],
+#   'hashvalue2': [
+# ...]
+#
+
+def compute_hashes(top=os.sep):
+    hashes = {}
+    for root, dirs, files in os.walk(top):
+	for f in files:
+            path = makepath(root, f)
+            m = md5(path)
+            s = os.path.getsize(path)
+
+            if m in hashes.keys():
+                hashes[m].append(path)
+            else:
+                hashes[m] = [path]
+
+    return hashes
+
+def main(argv):
+    hashes = compute_hashes(argv[1])
+
+    dups = [k for k,v in hashes.iteritems() if len(v) > 1]
+    for d in dups:
+        print "Possible duplicate files"
+        for f in hashes[d]:
+            print "\t" + f
+
+        print "\n"
+
+main(sys.argv)
